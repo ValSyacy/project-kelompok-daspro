ace9085973fcbe964657a04388dde28f02626a96
ValSyacy <novalsyacy11@gmail.com>
2023-09-25 10:53:42 +0700

    Update Menu

--- PrototypeSistemATM.java
+++ PrototypeSistemATM.java
@@ -6,30 +6,80 @@ public class PrototypeSistemATM {
         Scanner sc = new Scanner(System.in);
 
         int password = 123;
+        do {
+            System.out.println("\nMasukkan Password Anda: ");
+            password = sc.nextInt();
 
-        System.out.println("Masukkan Password Anda: ");
-        password = sc.nextInt();
+            if (password==123) {
+                System.out.println("\nSilahkan Pilih Menu Transaksi \n1.Tarik Tunai \n2.Transfer Saldo \n3.Pengecekan Bunga Tabungan\n");
+            
+                int menu;
+                menu = sc.nextInt();
 
-        if (password==123) {
-            System.out.println("\nSilahkan Pilih Menu Transaksi \n1.Tarik Tunai \n2.Transfer Saldo \n3.Pengecekan Bunga Tabungan\n");
-            int menu;
-            menu = sc.nextInt();
-            if (menu==1) {
-                System.out.println("\nMasukkan Jumlah Saldo Anda");
-                int saldo_awal, saldo_akhir, nominal_tariktunai;
-                saldo_awal=sc.nextInt();
-                System.out.println("\nMasukkan Nominal Jumlah Tarik Tunai");
-                nominal_tariktunai=sc.nextInt();
-                
-                saldo_akhir= saldo_awal-nominal_tariktunai;
+                switch(menu) {
+                    case 1:
 
-                System.out.println("\nTarik Tunai Berhasil Senilai\n" + nominal_tariktunai + "\nSisa Saldo Anda Sejumlah\n" + saldo_akhir + "\n");
-            } else {
-                System.out.println("\nMaaf Menu Lainnya Masih Dalam Pengembangan\n");
+                    int saldo_awal, saldo_akhir, nominal_tariktunai;
+                    
+                    System.out.println("\nMasukkan Jumlah Saldo Anda");
+                    saldo_awal=sc.nextInt();
+                    System.out.println("\nMasukkan Nominal Jumlah Tarik Tunai");
+                    nominal_tariktunai=sc.nextInt();
+                    
+                    saldo_akhir= saldo_awal-nominal_tariktunai;
+                    
+                    System.out.println("\n-----TARIK TUNAI BERHASIL-----");
+                    System.out.println("Tarik Tunai Berhasil Senilai : Rp." + nominal_tariktunai + "\nSisa Saldo Anda Sejumlah     : Rp." + saldo_akhir + "\n");
+                    break;
+
+                    case 2:
+
+                    int saldo_akhir2, saldo_awal2, nominal_transfer;
+                    String nama_penerima;
+                    long rekening_penerima;
+
+                    System.out.println("\nNama Penerima");
+                    nama_penerima=sc.next();
+
+                    System.out.println("\nMasukkan Rekening Penerima");
+                    rekening_penerima=sc.nextLong();
+
+                    System.out.println("\nMasukkan Jumlah Saldo Anda");
+                    saldo_awal2=sc.nextInt();
+
+                    System.out.println("\nMasukkan Nominal Transfer");
+                    nominal_transfer=sc.nextInt();
+                    
+                    saldo_akhir2= saldo_awal2-nominal_transfer;
+
+                    System.out.println("\n-----TRANSFER BERHASIL-----");
+                    System.out.println("Nama Penerima\t  : " +nama_penerima);
+                    System.out.println("Rekening Penerima : " +rekening_penerima);
+                    System.out.println("\nTranfer Berhasil Senilai : Rp." +nominal_transfer + "\nSisa Saldo Anda Sejumlah : Rp." + saldo_akhir2 + "\n");
+                    break;
+
+                    case 3:
+                    int jmlh_tabungan_awal, lama_tabungan;
+                    double prosentase_bunga =0.02, bunga, jmlh_tabungan_akhir;
+
+                    System.out.println("\nMasukkan Jumlah Tabungan Awal Anda");
+                    jmlh_tabungan_awal=sc.nextInt();
+                    System.out.println("\nMasukkan Lama Menabung Anda");
+                    lama_tabungan=sc.nextInt();
+
+                    bunga= lama_tabungan*prosentase_bunga*jmlh_tabungan_awal;
+                    jmlh_tabungan_akhir=bunga+jmlh_tabungan_awal;
+
+                    System.out.println("\nJumlah Tabungan Akhir Anda Adalah Senilai : Rp." +jmlh_tabungan_akhir +"\n");
+                    break;
+
+                    default:
+                    System.out.println("\nMaaf Menu Lainnya Masih Dalam Pengembangan\n");
+                }break;
+            
+            }else {
+                System.out.println("\nPassword Yang Anda Masukkan Salah\nSilahkan Masukkan Ulang Password Anda");
             }
-           
-        } else {
-            System.out.println("\nPassword Yang Anda Masukkan Salah\n");
-        }
+        }while(true);
     }
 }
