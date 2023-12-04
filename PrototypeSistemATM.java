8280969eef71f3f54b70028a410431fe384d085b
ValSyacy <novalsyacy11@gmail.com>
2023-12-04 10:06:40 +0700

    Penambahan fitur Cek Saldo dan Setor Tunai

--- PrototypeSistemATM.java
+++ PrototypeSistemATM.java
@@ -7,8 +7,8 @@ public class PrototypeSistemATM {
     private static Scanner scanner = new Scanner(System.in);
 
     // Fungsi untuk menampilkan menu
-    private static void displayMenu() {
-        String[] menu = {"1. Tarik Tunai", "2. Transfer Saldo", "3. Pengecekan Bunga Tabungan"};
+    private static void tampilkanMenu() {
+        String[] menu = {"1. Cek Saldo", "2. Tarik Tunai", "3. Setor Tunai", "4. Cek Bunga Tabungan", "0. Keluar"};
 
         System.out.println("\nMenu:");
         for (String menuItem : menu) {
@@ -16,28 +16,48 @@ public class PrototypeSistemATM {
         }
     }
 
+    // Fungsi untuk cek saldo
+    private static void cekSaldo(int saldoAwal) {
+        System.out.println("\n-----CEK SALDO-----");
+        System.out.println("Saldo Anda Saat Ini Adalah: Rp." + saldoAwal + "\n");
+    }
+
+    // Fungsi untuk setor tunai
+    private static int setorTunai(int saldoAwal) {
+        System.out.print("\nMasukkan Nominal Setor Tunai: Rp.");
+        int nominalSetorTunai = scanner.nextInt();
+        int saldoAkhirSetorTunai = saldoAwal + nominalSetorTunai;
+        
+        // Menampilkan hasil transaksi
+        System.out.println("\n-----SETOR TUNAI BERHASIL-----");
+        System.out.println("Setor Tunai Berhasil Senilai: Rp." + nominalSetorTunai);
+        System.out.println("Sisa Saldo Anda Sejumlah : Rp." + saldoAkhirSetorTunai + "\n");
+
+        return saldoAkhirSetorTunai;
+    }
+
     // Fungsi untuk meminta input password
-    private static int enterPassword() {
+    private static int masukkanPassword() {
         System.out.print("\nMasukkan Password Anda: ");
         return scanner.nextInt();
     }
 
     // Fungsi untuk melakukan tarik tunai
-    private static int withdrawCash(int saldoAwal) {
+    private static int tarikTunai(int saldoAwal) {
         System.out.print("\nMasukkan Nominal Jumlah Tarik Tunai: Rp.");
         int nominalTarikTunai = scanner.nextInt();
-        return saldoAwal - nominalTarikTunai;
-    }
+        int saldoAkhirTarikTunai = saldoAwal - nominalTarikTunai;
 
-    // Fungsi untuk melakukan transfer saldo
-    private static int transferBalance(int saldoAwal) {
-        System.out.print("\nMasukkan Nominal Transfer: Rp.");
-        int nominalTransfer = scanner.nextInt();
-        return saldoAwal - nominalTransfer;
+        // Menampilkan hasil transaksi
+        System.out.println("\n-----TARIK TUNAI BERHASIL-----");
+        System.out.println("Tarik Tunai Berhasil Senilai : Rp." + nominalTarikTunai);
+        System.out.println("Sisa Saldo Anda Sejumlah : Rp." + saldoAkhirTarikTunai + "\n");
+
+        return saldoAkhirTarikTunai;
     }
 
     // Fungsi untuk pengecekan bunga tabungan
-    private static double checkInterest() {
+    private static double cekBungaTabungan() {
         System.out.print("\nMasukkan Jumlah Tabungan Awal Anda: Rp.");
         int jumlahTabunganAwal = scanner.nextInt();
 
@@ -46,14 +66,13 @@ public class PrototypeSistemATM {
 
         double prosentaseBunga = 0.02;
         double bunga = lamaTabungan * prosentaseBunga * jumlahTabunganAwal;
-        return bunga + jumlahTabunganAwal;
-    }
+        double jumlahTabunganAkhir = bunga + jumlahTabunganAwal;
 
-    // Fungsi untuk menampilkan hasil transaksi
-    private static void displayTransactionResult(String transactionType, String additionalInfo, int saldoAkhir) {
-        System.out.println("\n-----" + transactionType + " BERHASIL-----");
-        System.out.println(additionalInfo);
-        System.out.println("Sisa Saldo Anda Sejumlah : Rp." + saldoAkhir + "\n");
+        // Menampilkan hasil transaksi
+        System.out.println("\n-----PENGECEKAN BUNGA TABUNGAN BERHASIL-----");
+        System.out.println("Jumlah Tabungan Akhir Anda Adalah Senilai : Rp." + jumlahTabunganAkhir + "\n");
+
+        return jumlahTabunganAkhir;
     }
 
     public static void main(String[] args) {
@@ -61,40 +80,41 @@ public class PrototypeSistemATM {
 
         // Perulangan Password
         do {
-            int password = enterPassword();
+            int password = masukkanPassword();
 
             if (password == PASSWORD) {
-                displayMenu();
+                tampilkanMenu();
                 int konfirmasiMenu;
 
-                System.out.print("\nSilahkan Menu Yang Ingin Anda Pilih: ");
+                System.out.print("\nSilakan Pilih Menu yang Anda Inginkan: ");
                 konfirmasiMenu = scanner.nextInt();
 
                 switch (konfirmasiMenu) {
-                    case 1: // Menu 1. Tarik Tunai
+                    case 1: // Menu 1. Cek Saldo
                         System.out.print("\nMasukkan Jumlah Saldo Anda: Rp.");
                         saldoAwal = scanner.nextInt();
-                        int saldoAkhirTarikTunai = withdrawCash(saldoAwal);
-
-                        // Menampilkan hasil transaksi
-                        displayTransactionResult("TARIK TUNAI", "Tarik Tunai Berhasil Senilai : Rp." + saldoAkhirTarikTunai, saldoAkhirTarikTunai);
+                        cekSaldo(saldoAwal);
                         break;
 
-                    case 2: // Menu 2. Transfer Saldo
+                    case 2: // Menu 2. Tarik Tunai
                         System.out.print("\nMasukkan Jumlah Saldo Anda: Rp.");
                         saldoAwal = scanner.nextInt();
-                        int saldoAkhirTransfer = transferBalance(saldoAwal);
-
-                        // Menampilkan hasil transaksi
-                        displayTransactionResult("TRANSFER", "Transfer Berhasil Senilai : Rp." + saldoAkhirTransfer, saldoAkhirTransfer);
+                        tarikTunai(saldoAwal);
                         break;
 
-                    case 3: // Menu 3. Pengecekan Bunga Tabungan
-                        double jumlahTabunganAkhir = checkInterest();
-
-                        // Menampilkan hasil transaksi
-                        displayTransactionResult("PENGECEKAN BUNGA TABUNGAN", "Jumlah Tabungan Akhir Anda Adalah Senilai : Rp." + jumlahTabunganAkhir, (int) jumlahTabunganAkhir);
+                    case 3: // Menu 3. Setor Tunai
+                        System.out.print("\nMasukkan Jumlah Saldo Anda: Rp.");
+                        saldoAwal = scanner.nextInt();
+                        saldoAwal = setorTunai(saldoAwal);
                         break;
+
+                    case 4: // Menu 4. Cek Bunga Tabungan
+                        cekBungaTabungan();
+                        break;
+
+                    case 0: // Menu 0. Keluar
+                        System.out.println("\nTerima kasih telah menggunakan layanan ATM. Sampai jumpa!");
+                        return;
 
                     default:
                         System.out.println("\nMaaf Menu Lainnya Masih Dalam Pengembangan\n");
@@ -102,13 +122,13 @@ public class PrototypeSistemATM {
                 }
 
                 // Konfirmasi Transaksi Lain
-                System.out.println("Apakah Anda Ingin Melakukan Transaksi Lainnya? (y/t)");
+                System.out.print("Apakah Anda Ingin Melakukan Transaksi Lainnya? (y/t) ");
                 String konfirmasiLain = scanner.next();
                 if (!konfirmasiLain.equalsIgnoreCase("y")) {
                     break;
                 }
             } else {
-                System.out.println("\nPassword Yang Anda Masukkan Salah\nSilahkan Masukkan Ulang Password Anda");
+                System.out.println("\nPassword yang Anda Masukkan Salah\nSilakan Masukkan Ulang Password Anda");
             }
         } while (true);
 
